40df7a3f78b29f67ac1a2b7e7b7c71437c348c6b
2F4S7 <nightsta1kerhf@gmail.com>
2021-03-23 07:00:11 +0200

    task-3

--- js/task-2.js
+++ js/task-2.js
@@ -6,3 +6,14 @@ const ingredients = [
     "Зелень",
     "Приправы",
 ];
+
+const productList = ingredients.map((ingredient) => {
+    const li = document.createElement("li");
+    li.textContent = ingredient;
+    console.log(li);
+    return li;
+});
+
+const productListEl = document.querySelector("#ingredients");
+productListEl.append(...productList);
+console.log(productListEl);
