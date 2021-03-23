48dc5ebb68f74f99fbae3ca45e8be6c63c4a7baf
2F4S7 <nightsta1kerhf@gmail.com>
2021-03-23 08:14:53 +0200

    task-4

--- js/task-2.js
+++ js/task-2.js
@@ -1,19 +1,19 @@
 const ingredients = [
-    "Картошка",
-    "Грибы",
-    "Чеснок",
-    "Помидоры",
-    "Зелень",
-    "Приправы",
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
 ];
 
 const productList = ingredients.map((ingredient) => {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.textContent = ingredient;
     console.log(li);
     return li;
 });
 
-const productListEl = document.querySelector("#ingredients");
+const productListEl = document.querySelector('#ingredients');
 productListEl.append(...productList);
 console.log(productListEl);
--- js/task-3.js
+++ js/task-3.js
@@ -1,22 +1,22 @@
 const images = [{
-        url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        alt: "White and Black Long Fur Cat",
+        url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'White and Black Long Fur Cat',
     },
     {
-        url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+        url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
     },
     {
-        url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        alt: "Group of Horses Running",
+        url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'Group of Horses Running',
     },
 ];
 
-const imagesListEl = document.querySelector("#gallery");
+const imagesListEl = document.querySelector('#gallery');
 const listMarkUp = ({ url, alt }) => {
     return `<li><img src=${url} alt=${alt} width = 600 height = 450/></li>`;
 };
 
-const imageListMarkup = images.map(listMarkUp).join("");
+const imageListMarkup = images.map(listMarkUp).join('');
 console.log(imageListMarkup);
-imagesListEl.insertAdjacentHTML("beforeend", imageListMarkup);
+imagesListEl.insertAdjacentHTML('beforeend', imageListMarkup);
