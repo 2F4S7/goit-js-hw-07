f4afb60ffe62bbba4d6e246a43b64b0faf9225d0
2F4S7 <nightsta1kerhf@gmail.com>
2021-03-23 13:00:27 +0200

    JS-HW-07

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,11 @@
-const summItemsEl = document.querySelectorAll(".item");
+const summItemsEl = document.querySelectorAll('.item');
 console.log(summItemsEl);
 const viewCategories = () =>
     console.log(`В списке ${summItemsEl.length} категории.`);
 viewCategories(summItemsEl);
 
 const viewCategory = summItemsEl.forEach((item) => {
-    const categoriesItemEl = item.querySelectorAll("li");
+    const categoriesItemsEl = item.querySelectorAll('li');
     console.log(`Ктегория: ${item.firstElementChild.textContent}
     Количество элементов: ${categoriesItemsEl.length}`);
 });
